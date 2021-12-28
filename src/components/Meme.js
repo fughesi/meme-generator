cc8dd3b4d91abda505b41825a8b7d9dc7e148649
Steven <steven.camillieri@protonmail.com>
2021-12-28 03:04:41 -0500

    completed layout of imput elements

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,17 @@
 
 
 export default function Meme() {
+
+    function noReload(e) {
+        return e.preventDefault()
+    }
+
     return (
         <div id="memeContainer">
-            <form>
+            <form id="formEl">
                 <input type="text"  placeholder="top text" />
                 <input type="text"  placeholder="bottom text" />
-                <div id="submitContainer">
-                    <input type="submit" value="Get a new meme image" />
-                </div>
+                <input type="submit" value="Get a new meme image" id="btn" onClick={noReload} />
             </form>
         </div>
     )
