251df069bffd36cc1858b5d3901e089cc205e563
Steven <steven.camillieri@protonmail.com>
2021-12-28 03:38:01 -0500

    starting on meme photos on new branch

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,18 +1,25 @@
-
+import Data from "../data/memesData"
 
 export default function Meme() {
-
-    function noReload(e) {
-        return e.preventDefault()
+    
+    function newMeme(e) {
+        e.preventDefault()
+        let num = Math.ceil(Math.random() * 100)
+        console.log(num)
     }
 
     return (
+        <>
         <div id="memeContainer">
             <form id="formEl">
                 <input type="text"  placeholder="top text" />
                 <input type="text"  placeholder="bottom text" />
-                <input type="submit" value="Get a new meme image" id="btn" onClick={noReload} />
+                <input type="submit" value="Get a new meme image" id="btn" onClick={newMeme} />
             </form>
         </div>
+        <div id="photoContainer">
+            
+        </div>
+        </>
     )
 }
