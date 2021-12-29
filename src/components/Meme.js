a64b1e1144e6c677203ee36bcfbae1d46dcea779
Steven <steven.camillieri@protonmail.com>
2021-12-28 21:25:59 -0500

    starting with state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,25 +1,31 @@
+import React, {useState} from "react"
 import Data from "../data/memesData"
 
 export default function Meme() {
+    // state variables
+    const [memeImage, setMemeImage] = useState("")
+    // regular variables
+    const array = Data.data.memes
     
-    
+    //gets a new photo displayed from the Data
     function newMeme(e) {
         e.preventDefault()
-        let num = Math.ceil(Math.random() * 100)
-        const photos = Data.data.memes[num].url
-        console.log(photos)
-        return photos
+        let num = Math.floor(Math.random() * array.length)
+        const photos = array[num].url
+        setMemeImage(i => photos)
     }
 
     return (
         <>
         <div id="memeContainer">
             <form id="formEl">
-                <input type="text"  placeholder="top text" />
-                <input type="text"  placeholder="bottom text" />
+                <input type="text"  placeholder="top text" id="topText" />
+                <input type="text"  placeholder="bottom text" id="bottomText" />
                 <input type="submit" value="Get a new meme image" id="btn" onClick={newMeme} />
-                <img src="https://i.imgflip.com/1g8my4.jpg" alt="something" />
             </form>
+            <div id="photoContainer">
+                <img src={memeImage} alt="this is your meme" />
+            </div>
         </div>
         </>
     )
